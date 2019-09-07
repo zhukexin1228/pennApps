4f91efab659257cb47a1594d443c4037ec89220c
KexinZhu1228 <zhukexin@seas.upenn.edu>
2019-09-07 12:35:06 -0400

    price with state

--- getPriceValue.py
+++ getPriceValue.py
@@ -1,5 +1,22 @@
+import collections
 import pandas as pd
-apt_df = pd.read_csv('apt_price.csv')
-avgNum = apt_df.groupby('State').mean()
-minPrice = apt_df.groupby('State').min()
-print(avgNum)
+
+f = open('apt_price.csv')
+states = collections.defaultdict(list)
+i = 0
+for line in f:
+    if i == 0:
+        i += 1
+        continue
+    lst = line.split(',')
+    states[lst[0]].append(float(lst[1]))
+    i += 1
+f.close()
+
+info = {}
+for key, val in states.items():
+    if key not in info:
+        info[key] = {}
+    info[key]["max"] = max(val)
+    info[key]["min"] = min(val)
+    info[key]["avg"] = sum(val) / len(val)
