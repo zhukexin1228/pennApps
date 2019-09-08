74346e01ed6bca6e34a5f2fa1769b312a31b29db
KexinZhu1228 <zhukexin@seas.upenn.edu>
2019-09-07 22:46:47 -0400

    websocket

--- backend/web.py
+++ backend/web.py
@@ -5,6 +5,8 @@ import json
 import collections
 import pandas as pd
 import math
+import asyncio
+import websockets
 
 
 f = open('apt_price.csv')
@@ -32,46 +34,48 @@ def getIndex(wage, state):
     return math.floor(10 - info[state]['avg'] / wage * 10)
 
 
-BUFFER_SIZE = 1024
+async def echo(websocket, path):
+    async for message in websocket:
+        data = await websocket.recv()
 
-port = 5000
+        loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
+        title_avg_dict_file = open('title_avg_dict.json', 'r')
+        title_avg_dict = json.load(title_avg_dict_file)
+        level_avg_dict_file = open('level_avg_dict.json', 'r')
+        level_avg_dict = json.load(level_avg_dict_file)
+        state_avg_dict_file = open('state_avg_dict.json', 'r')
+        state_avg_dict = json.load(state_avg_dict_file)
 
-s = socket(AF_INET, SOCK_STREAM)
-s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-s.bind(('localhost', port))
-s.listen(1)
+        requests = json.load(data)
+        jobTitle = requests["jobtitle"]
+        state = requests["location"]
+        level = requests["level"]
 
-conn, addr = s.accept()
+        xTest = [[title_avg_dict[jobTitle],
+                  level_avg_dict[level], state_avg_dict[state]]]
+        yPrediction = loaded_model.predict(xTest)
+
+        res["predictWage"] = yPrediction[0]
+        res["max"] = info[state]['max']
+        res["min"] = info[state]['min']
+        res['avg'] = info[state]['avg']
+        res["index"] = getIndex(yPrediction[0], state)
+        resJson = json.dumps(res)
+        await websocket.send(res)
+
+
+# s = socket(AF_INET, SOCK_STREAM)
+# s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+# s.bind(('localhost', port))
+# s.listen(1)
+
+
 while True:
-    data = conn.recv(BUFFER_SIZE)
-    
-    if not data or data[0:3] == 'GET':
-        continue
+
     print("data", data)
 
-    loaded_model = pickle.load(open('finalized_model.sav', 'rb'))
-    title_avg_dict_file = open('title_avg_dict.json', 'r')
-    title_avg_dict = json.load(title_avg_dict_file)
-    level_avg_dict_file = open('level_avg_dict.json', 'r')
-    level_avg_dict = json.load(level_avg_dict_file)
-    state_avg_dict_file = open('state_avg_dict.json', 'r')
-    state_avg_dict = json.load(state_avg_dict_file)
+    # conn.sendAll(res)
+    start_server = websockets.serve(echo, "localhost", 5000)
 
-    requests = json.load(data)
-    jobTitle = requests["jobtitle"]
-    state = requests["location"]
-    level = requests["level"]
-
-    xTest = [[title_avg_dict[jobTitle],
-              level_avg_dict[level], state_avg_dict[state]]]
-    yPrediction = loaded_model.predict(xTest)
-
-    res = {}
-    res["predictWage"] = yPrediction[0]
-    res["max"] = info[state]['max']
-    res["min"] = info[state]['min']
-    res['avg'] = info[state]['avg']
-    res["index"] = getIndex(yPrediction[0], state)
-    resJson = json.dumps(res)
-
-    conn.sendAll(res)
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
